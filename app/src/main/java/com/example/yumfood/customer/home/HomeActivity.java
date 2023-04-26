9285ea5f7cae142eb6cdb77694f8ee631e91ca63
Thái Tăng Đức <thaitangduc1@gmail.com>
2023-04-27 00:43:14 +0700

    end UI selector

---
+++ app/src/main/java/com/example/yumfood/customer/home/HomeActivity.java
@@ -0,0 +1,35 @@
+package com.example.yumfood.customer.home;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import com.example.yumfood.R;
+
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+
+public class HomeActivity extends AppCompatActivity {
+
+    BottomNavigationView btnBottom_Nav_HomeID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        btnBottom_Nav_HomeID = findViewById(R.id.bottom_nav_home);
+        btnBottom_Nav_HomeID.setSelectedItemId(R.id.nav_home);
+
+    }
+    private void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.framelayout_home, fragment);
+        fragmentTransaction.commit();
+    }
+}
+
