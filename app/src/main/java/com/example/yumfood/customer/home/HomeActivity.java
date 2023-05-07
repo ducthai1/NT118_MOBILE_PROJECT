1f0a7c1cc1a82a6d097c54308393ae83ed149636
Thái Tăng Đức <thaitangduc1@gmail.com>
2023-05-07 22:01:57 +0700

    Interact_customer_and_fix_constraint

--- app/src/main/java/com/example/yumfood/customer/home/HomeActivity.java
+++ app/src/main/java/com/example/yumfood/customer/home/HomeActivity.java
@@ -9,11 +9,15 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import com.example.yumfood.R;
 
-
+import com.example.yumfood.customer.home.notification.CustomerNotification;
+import com.example.yumfood.customer.favourite.CustomerFavourite;
 import com.example.yumfood.customer.home.homepage.HomeFragment;
+import com.example.yumfood.customer.home.personal.CustomerPersonal;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
-
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.example.yumfood.customer.favourite.CustomerFavourite;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -27,6 +31,29 @@ public class HomeActivity extends AppCompatActivity {
         setFragment(new HomeFragment());
         btnBottom_Nav_HomeID.setSelectedItemId(R.id.nav_home);
 
+        btnBottom_Nav_HomeID.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        setFragment(new HomeFragment());
+                        return true;
+//                    case R.id.nav_my_ords:
+//                        setFragment(new MyOrderFragment());
+//                        return true;
+                    case R.id.nav_likes:
+                        setFragment(new CustomerFavourite());
+                        return true;
+                    case R.id.nav_me:
+                        setFragment(new CustomerPersonal());
+                        return true;
+                    case R.id.nav_notifications:
+                        setFragment(new CustomerNotification());
+                        return true;
+                }
+                return false;
+            }
+        });
     }
     private void setFragment(Fragment fragment){
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
---
+++ app/src/main/java/com/example/yumfood/customer/home/personal/CustomerPersonal.java
@@ -0,0 +1,23 @@
+package com.example.yumfood.customer.home.personal;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+
+import com.example.yumfood.R;
+
+public class CustomerPersonal extends Fragment{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.activity_personal, container, false);
+        return root;
+    }
+}
